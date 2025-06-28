f135ac74aa7764cf6f5e43be62d16acab9ed7003
Dmitry A. Markelov <dmarkelo89@gmail.com>
2025-06-28 12:17:58 -0500

    Fix timeUtils.ts and deploy application

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -8,8 +8,6 @@ export interface TimeOfDay {
   shouldAutoStart: boolean;
 }
 
-export const getTimeOfDay = (userLocation?: { lat: number; lng: number }): TimeOfDay => {
-}
 export const getTimeOfDay = (): TimeOfDay => {
   const now = new Date();
   const hour = now.getHours();
