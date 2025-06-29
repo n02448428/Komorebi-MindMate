770f671f08df0333eeb6b6d6b3a32312bc12a910
Dmitry A. Markelov <dmarkelo89@gmail.com>
2025-06-29 11:14:18 -0500

    Remove bubble styling from privacy notice

--- src/pages/InsightsGallery.tsx
+++ src/pages/InsightsGallery.tsx
@@ -351,10 +351,10 @@ const InsightsGallery: React.FC = () => {
 
       {/* Privacy Notice - Bottom of page */}
       <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-[5]">
-        <p className={`text-xs px-3 py-1 rounded-full backdrop-blur-sm border ${
+        <p className={`text-xs ${
           timeOfDay.period === 'morning' 
-            ? 'bg-gray-800/80 text-gray-200 border-gray-700/50' 
-            : 'bg-white/80 text-gray-800 border-white/50'
+            ? 'text-white' 
+            : 'text-gray-900'
         }`}>
           🔒 All data stored locally & privately on your device
         </p>
