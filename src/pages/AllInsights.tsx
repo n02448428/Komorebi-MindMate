8fd33f7e3d818ee2efde599fbe31730be71e20b6
Dmitry A. Markelov <dmarkelo89@gmail.com>
2025-06-30 10:04:23 -0500

    Fix syntax error in AllInsights.tsx

--- src/pages/AllInsights.tsx
+++ src/pages/AllInsights.tsx
@@ -276,4 +276,13 @@ const AllInsights: React.FC = () => {
       {/* Privacy Notice - Bottom of page */}
       <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-[5]">
         <p className={`text-[10px] sm:text-xs whitespace-nowrap ${
-          timeOfDay.perio
+          timeOfDay.period === 'morning' ? 'text-gray-600' : 'text-gray-400'
+        }`}>
+          Your conversations are private and secure
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AllInsights;
