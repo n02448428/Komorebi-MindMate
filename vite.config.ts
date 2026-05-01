afd707cba3c53ea2716436087bb5063070abaec3
Dmitry A. Markelov <dmarkelo89@gmail.com>
2026-05-01 10:55:26 -0700

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Komorebi-MindMate/',
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        manualChunks: { 
-          vendor: ['react', 'react-dom', 'framer-motion'] 
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'framer-motion']
         }
       }
     }
