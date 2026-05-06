536cca60dab588a2088f3cb893696ca17d2f18e3
Dmitry A. Markelov <dmarkelo89@gmail.com>
2026-05-05 18:55:25 -0700

    Fix base path: change /Komorebi-MindMate/ to /session/ for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Komorebi-MindMate/',
+    base: '/session/',
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 2000,
